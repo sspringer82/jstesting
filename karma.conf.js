3fbadb16771954c5c330164a27b54501a7a3a295
Sebastian Springer <info@sebastian-springer.com>
2018-03-20 11:09:33 +0100

    random true

--- karma.conf.js
+++ karma.conf.js
@@ -49,5 +49,11 @@ module.exports = function(config) {
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity,
+
+    client: {
+      jasmine: {
+        random: true,
+      },
+    },
   });
 };
