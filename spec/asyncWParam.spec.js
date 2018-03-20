c4ea3df4aa701c0e0780de21059aaa16dae5043c
Sebastian Springer <info@sebastian-springer.com>
2018-03-20 12:28:06 +0100

    async test

--- spec/asyncWParam.spec.js
+++ spec/asyncWParam.spec.js
@@ -1,20 +1,21 @@
-describe('Async function', function () {
-  describe('with fake timers', function () {
+describe('Async function', () => {
+  describe('with fake timers', () => {
     beforeEach(() => jasmine.clock().install());
     afterEach(() => jasmine.clock().uninstall());
 
-    it('should call a callback after 400ms with "Guten Tach"', function () {
-      const inputValue = 'Guten Tach'; 
-      const timeoutValue = 400; 
-      
-      const callback = (value) => {
+    it('should call a callback after 400ms with "Guten Tach"', done => {
+      const inputValue = 'Guten Tach';
+      const timeoutValue = 400;
+
+      const callback = value => {
         expect(value).toEqual(inputValue);
-      }
+        done();
+      };
       async(callback, inputValue, timeoutValue);
       jasmine.clock().tick(400);
-    })
+    });
 
-    it('should call a callback using spies', function () {
+    it('should call a callback using spies', () => {
       const callback = jasmine.createSpy('My fancy callback');
 
       async(callback, 'foo', 400);
@@ -22,33 +23,33 @@ describe('Async function', function () {
 
       expect(callback).toHaveBeenCalled();
     });
-  
-    it('should call another callback using spies', function () {
+
+    it('should call another callback using spies', () => {
       const inputValue = 'foo';
       const callback = jasmine.createSpy('blubber');
-  
+
       async(callback, inputValue, 400);
       jasmine.clock().tick(400);
 
       expect(callback).toHaveBeenCalledWith(inputValue);
     });
-  })
+  });
 
-  xdescribe('without fake timers', function () {
-    it('should call a callback after 400ms with "Guten Tach"', function (done) {
-      const inputValue = 'Guten Tach'; 
-      const timeoutValue = 400; 
-      
-      const callback = (value) => {
+  // commented out cause it takes way to much time
+  xdescribe('without fake timers', () => {
+    it('should call a callback after 400ms with "Guten Tach"', done => {
+      const inputValue = 'Guten Tach';
+      const timeoutValue = 400;
+
+      const callback = value => {
         expect(value).toEqual(inputValue);
         done();
-      }
-      
+      };
+
       async(callback, inputValue, timeoutValue);
-    })
-  
-  
-    it('should call a callback using spies', function (done) {
+    });
+
+    it('should call a callback using spies', done => {
       const callback = jasmine.createSpy('My fancy callback');
       async(callback, 'foo', 400);
       setTimeout(() => {
@@ -56,19 +57,18 @@ describe('Async function', function () {
         done();
       }, 401);
     });
-  
-    it('should call another callback using spies', function (done) {
+
+    it('should call another callback using spies', done => {
       const inputValue = 'foo';
       const callback = jasmine.createSpy('blubber');
-  
-      callback.and.callFake(function (v) {
+
+      callback.and.callFake(v => {
         expect(v).toEqual(inputValue);
         expect(callback).toHaveBeenCalledWith(inputValue);
         done();
       });
-      
+
       async(callback, inputValue, 400);
     });
-  })
-  
-})
+  });
+});
